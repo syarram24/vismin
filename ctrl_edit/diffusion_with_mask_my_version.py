369c7bc089b112469675a2ef2492a2097e0029b2
syarram24 <syarramcorp@gmail.com>
2025-01-20 11:37:18 +0530

    +1

--- ctrl_edit/diffusion_with_mask_my_version.py
+++ ctrl_edit/diffusion_with_mask_my_version.py
@@ -678,9 +678,10 @@ class ImageEditProcessor:
         #         image_path = get_coco_path_by_image_id(split=coco_split, image_id=image_id)
         #     logger.info(f"Processing image id: {image_id}, image caption: {caption_text}")
         #     output_dir = self.get_output_dir_path_by_image_id(output_dir_root, image_id)
-        for idx, edited_obj_sample in tqdm(enumerate(edited_object_data), desc="Editing images") :
+        for idx, source_image_id in tqdm(enumerate(edited_object_data.keys()), desc="Editing images") :
             
             #if sample['category'] == 'object':
+            edited_obj_sample = edited_object_data[source_image_id]
             image_id = edited_obj_sample['source_image_id']
             caption_text = coco_original_data[image_id]['caption']
             coco_split = "val" if self.split == "validation" else self.split
